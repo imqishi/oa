294ae4cb61d4c41099dcd3e31b096510b0701d36
imqishi <imqishi@hotmail.com>
2017-10-01 19:15:31 +0800

    modify readme and default setting

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $env = require(__DIR__ . '/.env.php');
 $host = isset($env['DB_HOST']) ? $env['DB_HOST'] : 'localhost';
-$db = isset($env['DB_NAME']) ? $env['DB_NAME'] : 'yii_test';
+$db = isset($env['DB_NAME']) ? $env['DB_NAME'] : 'oa';
 $user = isset($env['DB_USER']) ? $env['DB_USER'] : 'root';
 $pwd = isset($env['DB_PASSWORD']) ? $env['DB_PASSWORD'] : '';
 
